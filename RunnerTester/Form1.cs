75a0c480930685d54ebbefa2aa83dff06a81d00a
jeandreatta <jean@zerodezoitosistemas.com.br>
2023-01-03 13:04:49 -0300

    Adicionar arquivos de projeto.

---
+++ RunnerTester/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RunnerTester
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SkiaSharpCOM.SkiaRenderer renderer = new SkiaSharpCOM.SkiaRenderer();
+            renderer.CreateBitmap(200, 200);
+            renderer.DrawCircle(50, 50, 25, 0, 2, true);
+
+            pictureBox1.Image = Microsoft.VisualBasic.Compatibility.VB6.Support.IPictureDispToImage(renderer.ToPicture());
+
+
+        }
+    }
+}
---
+++ SkiaSharpCOM/SkiaRenderer.cs
@@ -0,0 +1,53 @@
+﻿using SkiaSharp;
+using System.Runtime.InteropServices;
+using System.Drawing;
+using System.Drawing.Imaging;
+using stdole;
+using System.Windows.Forms;
+using System.IO;
+
+namespace SkiaSharpCOM
+{ 
+
+    [ComVisible(true)]
+    public class SkiaRenderer
+    {
+        private SKBitmap _bitmap;
+        public SkiaRenderer() { }
+        
+        public void CreateBitmap(int width, int height)
+        {
+            _bitmap = new SKBitmap(width, height);
+        }
+
+        [ComVisible(true)]
+        public void DrawCircle(float x, float y, float radius, int color, float strokeWidth, bool isAntialias)
+        {
+            // Create a new SKCanvas object to draw on the bitmap
+            using (SKCanvas canvas = new SKCanvas(_bitmap))
+            {
+                var cor = ColorTranslator.FromOle(color);
+                
+                // Create an SKPaint object to specify the circle's appearance
+                SKPaint paint = new SKPaint
+                {
+                    Color = new SKColor((uint)cor.ToArgb()),
+                    StrokeWidth = strokeWidth,
+                    IsAntialias = isAntialias
+                };
+                canvas.DrawCircle(x, y, radius, paint);
+            }
+        }
+
+        [ComVisible(true)]
+        public IPictureDisp ToPicture()
+        {
+            SKImage image = SKImage.FromPixels(_bitmap.PeekPixels());
+
+            SKData encoded = image.Encode();
+            Stream stream = encoded.AsStream();
+            var img = Image.FromStream(stream);
+            return (IPictureDisp)Microsoft.VisualBasic.Compatibility.VB6.Support.ImageToIPictureDisp(img);
+        }
+    }
+}
